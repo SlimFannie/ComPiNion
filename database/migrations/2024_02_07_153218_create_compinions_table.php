39ade0dbc2d9e238edbf00a30b6ad28e9a4a03b4
Fannie Hamel Thibault <90933505+SlimFannie@users.noreply.github.com>
2024-02-07 10:56:10 -0500

    Initial commit

---
+++ database/migrations/2024_02_07_153218_create_compinions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('compinions', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->foreignId('user_id')->constrained();
+            $table->string('nom');
+            $table->integer('jours')->default(0);
+            $table->integer('experience')->default(0);
+            $table->integer('merite')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('compinions');
+    }
+};
