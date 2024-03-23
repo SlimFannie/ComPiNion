18555a4fe4f7fffb57f1b49ebd3ad04d007a37ca
Fannie Hamel Thibault <90933505+SlimFannie@users.noreply.github.com>
2024-03-23 13:42:06 -0400

    Rollback des mutateurs de date lol

--- app/Models/Chaine.php
+++ app/Models/Chaine.php
@@ -14,33 +14,6 @@ class Chaine extends Model
         'end_date',
     ];
 
-    /* Mutateur pour les champs de date (voir explication plus détaillée au sujet des mutateurs dans le model User) 
-    Cela me permet de formatter les dates correctement. */
-
-    public function setStartDateAttribute($value)
-    {
-
-        $date = new \DateTime($value);
-
-        $this->attributes['start_date'] = [
-            'year' => $date->format('Y'),
-            'month' => $date->format('m'),
-            'day' => $date->format('d')
-        ];
-    }
-
-    public function setEndDateAttribute($value)
-    {
-
-        $date = new \DateTime($value);
-
-        $this->attributes['end_date'] = [
-            'year' => $date->format('Y'),
-            'month' => $date->format('m'),
-            'day' => $date->format('d')
-        ];
-    }
-
     public function user()
     {
         return $this->belongsTo(User::class);
