bf78d26dfdb92136aac1afe4427907dae614c862
Fannie Hamel Thibault <90933505+SlimFannie@users.noreply.github.com>
2024-03-23 10:40:09 -0400

    Update CharacterController.php

--- app/Http/Controllers/API/CharacterController.php
+++ app/Http/Controllers/API/CharacterController.php
@@ -21,7 +21,7 @@ class CharacterController extends Controller
 
     //GET - Compinion de l'usager
     public function showCharacter($id) {
-        $character = Character::find($id)->get();
+        $character = Character::find($id)->first();
         return $this->sendResponse($character, 'Le personnage a été trouvé avec succès.');
     }
 
