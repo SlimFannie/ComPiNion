93b3957bbd811759cbef8aff1b4b0b73c9bb69d3
Fannie Hamel Thibault <90933505+SlimFannie@users.noreply.github.com>
2024-03-27 08:36:42 -0400

    Update loggedUser interface

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -125,7 +125,7 @@ class UsersController extends Controller
                 $loggeduser = Auth::user();
                 if ($loggeduser->admin == true) {
                     Log::debug("Connexion réussi");
-                    return redirect()->with($loggeduser)->route('users.accueil');
+                    return redirect()->route('users.accueil')->with($loggeduser);
                 } 
             } else {
             //  Log::debug(Auth::attempt(['email'=> $request->email, 'password' => $request->password]));
